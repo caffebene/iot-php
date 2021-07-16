48b3dd0189bbc5a1fd5bf46279bf15927a09b1e1
caffebene <379264613@qq.com>
2021-07-16 21:09:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>openlog</h1>
+
 <?php
-echo sprintf("Hello World!");
+$file = fopen("/log/raw_post_data.log","w");
+echo fwrite($file,date("Y-m-d H:i:s"));
+fclose($file);
+?>
+
+</body>
+</html>

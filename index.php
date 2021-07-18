ce664580cf33226d0d14d797bb0d5537124cd107
caffebene <hzhhzh20@gmail.com>
2021-07-18 15:59:39 +0800

    0718-7

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
 <?php
 //echo file_get_contents("./raw_post_data.log");
 
-$file = fopen("raw_post_data.log","w");
-echo $file;
-fclose($file);
+$file = fopen("raw_post_data.log","r") or die("Unable to open file!");
+// 输出单行直到 end-of-file
+while(!feof($file)) {
+    echo fgets($file) . "<br>";
+  }
+  fclose($file);
 
 ?>
 

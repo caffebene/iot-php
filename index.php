ac0950e905a16e0c62c7b4304a6bcf3f24f37cf5
caffebene <hzhhzh20@gmail.com>
2021-07-18 15:42:33 +0800

    0718-3

--- index.php
+++ index.php
@@ -5,7 +5,12 @@
 <h1>openlog</h1>
 
 <?php
-echo file_get_contents("./raw_post_data.log");
+//echo file_get_contents("./raw_post_data.log");
+
+$file = fopen("/log/raw_post_data.log","w");
+echo fwrite($file,date("Y-m-d H:i:s"));
+fclose($file);
+
 ?>
 
 </body>

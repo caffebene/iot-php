f1416d45c6c58c98860d11a2eacd928b1f2600a5
caffebene <hzhhzh20@gmail.com>
2021-07-18 20:11:40 +0800

    0718-9

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <p>步骤1：通过物联网开发平台的<a href="https://cloud.tencent.com/document/product/1081/34739">产品开发</a>功能完成产品的开发；</p>
 <p>步骤2：通过物联网开发平台的<a href="https://cloud.tencent.com/document/product/1081/40292">数据开发</a>或<a href="https://cloud.tencent.com/document/product/1081/40298">数据同步</a>功能将产品状态同步至如下url：</p>
 <script>
-document.write(document.URL+"/post.php");
+document.write(document.URL+"post.php");
 </script>
 <p>步骤3：通过<a href="https://cloud.tencent.com/document/product/1081/34741">虚拟设备调试</a>功能或绑定真实设备完成设备数据上报。</p>
 <p>完成上述步骤后，即可于下方查看由物联网开发平台所转发的设备状态数据。</p>

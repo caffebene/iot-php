6812dcd4a2913ce21a94919e55eff5057c7e9036
caffebene <hzhhzh20@gmail.com>
2021-07-18 20:06:32 +0800

    0718-8

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <!DOCTYPE html>
 <html>
+<head> 
+<meta charset="utf-8"> 
+<title>Enable应用托管Demo</title>
+<head> 
 <body>
-
-<h1>openlog</h1>
-
+<h1 style="text-align:center;">欢迎使用物联使能应用托管Demo</h1>
+<h2>前置步骤</h2>
+<p>本示例将输出由物联网开发平台所转发的设备状态数据。请确认完成如下前置步骤：</p>
+<p>步骤1：通过物联网开发平台的<a href="https://cloud.tencent.com/document/product/1081/34739">产品开发</a>功能完成产品的开发；</p>
+<p>步骤2：通过物联网开发平台的<a href="https://cloud.tencent.com/document/product/1081/40292">数据开发</a>或<a href="https://cloud.tencent.com/document/product/1081/40298">数据同步</a>功能将产品状态同步至如下url：</p>
+<script>
+document.write(document.URL+"/post.php");
+</script>
+<p>步骤3：通过<a href="https://cloud.tencent.com/document/product/1081/34741">虚拟设备调试</a>功能或绑定真实设备完成设备数据上报。</p>
+<p>完成上述步骤后，即可于下方查看由物联网开发平台所转发的设备状态数据。</p>
+<h2>Post记录</h2>
+<button onClick="document.location.reload()">刷新</button>
 <?php
-//echo file_get_contents("./raw_post_data.log");
-
-$file = fopen("raw_post_data.log","r") or die("Unable to open file!");
+$file = fopen("raw_post_data.txt","r") or die("Unable to open file!");
 // 输出单行直到 end-of-file
 while(!feof($file)) {
     echo fgets($file) . "<br>";
   }
-  fclose($file);
-
+fclose($file);
 ?>
-
 </body>
 </html>
--- post.php
+++ post.php
@@ -2,37 +2,15 @@
 <html>
 <body>
 
-<h1>Enable Test</h1>
+<h1>接收Post数据</h1>
 
 <?php
-ini_set('display_errors',1);            //错误信息  
-ini_set('display_startup_errors',1);    //php启动错误信息  
-error_reporting(-1);                    //打印出所有的 错误信息  
-
-// 获取时间戳
-function microtime_float()
-{
-       list($usec, $sec) = explode(" ", microtime());
-       return ((float)$usec + (float)$sec);
-}
-function microtime_format($tag, $time)
-{
-       list($usec, $sec) = explode(".", $time);
-       $date = date($tag,$usec);
-       return str_replace('x', $sec, $date);
-}
-$time = microtime_float();
-$strtime = microtime_format('Ymd-His.x ', $time);
-
 // 接收Post数据
 $raw_post_data = file_get_contents('php://input');
-
 // 输出数据
 echo $strtime , '#post body:' , $raw_post_data , "\r\n";
-
 // 记录信息
-// file_put_contents("./raw_post_data.txt","$raw_post_data\r\n",FILE_APPEND);
-file_put_contents("raw_post_data.log", "$strtime DATA: $raw_post_data\r\n", FILE_APPEND | LOCK_EX);
+file_put_contents("raw_post_data.txt", "$strtime date("Y-m-d H:i:s"): $raw_post_data\r\n", FILE_APPEND | LOCK_EX);
 ?>
 
 
